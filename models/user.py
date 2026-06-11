1f134ddf45764e3502d9f658fe22d78dce2a49ce
Sharon Moegi <sharamoegi@gmail.com>
2026-06-11 16:47:25 +0300

    save new users to database with uniqueness check

--- models/user.py
+++ models/user.py
@@ -7,10 +7,18 @@ class User(Person):
         super().__init__(name, email)
         self.role = role
 
-        
+
     @classmethod
     def create(cls, name: str, email: str, role: str) -> "User":
         if not name or "@" not in email:
-            raise ValueError("Invalid credentials profile template.")
+            raise ValueError("Error: Invalid credentials profile .")
         if role not in ["client", "tasker"]:
-            raise ValueError("Role must be exactly 'client' or 'tasker'.")
+            raise ValueError("Error: Role must be exactly 'client' or 'tasker'.")
+        
+        db = load_db()
+        if name in db["users"]:
+            raise KeyError(f"A user named '{name}' already exists.")
+            
+        db["users"][name] = {"email": email, "role": role}
+        save_db(db)
+        return cls(name, email, role)
