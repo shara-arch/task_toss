a51104e1a2b8f94246d9ff9a2e5af8cee43e9414
Sharon Moegi <sharamoegi@gmail.com>
2026-06-11 16:43:59 +0300

    Create 'add user' method

--- models/user.py
+++ models/user.py
@@ -6,3 +6,11 @@ class User(Person):
     def __init__(self, name: str, email: str, role: str):
         super().__init__(name, email)
         self.role = role
+
+        
+    @classmethod
+    def create(cls, name: str, email: str, role: str) -> "User":
+        if not name or "@" not in email:
+            raise ValueError("Invalid credentials profile template.")
+        if role not in ["client", "tasker"]:
+            raise ValueError("Role must be exactly 'client' or 'tasker'.")
