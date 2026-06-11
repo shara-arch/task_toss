1e578739162543acf8d995997df9e63e56c58956
Sharon Moegi <sharamoegi@gmail.com>
2026-06-11 19:02:03 +0300

    Create Command handler for adding projects

--- main.py
+++ main.py
@@ -8,10 +8,18 @@ from rich.console import Console #Used to make the Comand line visually appealin
 from rich.table import Table #Will be used to print table of projects/taks
 
 console = Console()
-#Command handler for creating users
+#Command handler for CREATING USERS
 def handle_add_user(args):
     try:
         user = User.create(args.name, args.email, args.role)
-        console.print(f"[bold green] Success !:Created profile for {user}[/bold green] ")
+        console.print(f"[bold green] Success! :[/bold green]Created profile for {user} ")
+    except Exception as e:
+        console.print(f"[bold red] Operational Error:[/bold red] {e}")
+
+#Command handler for ADDING PROJECTS
+def handle_add_project(args):
+    try:
+        project = Project.create(args.title, args.desc, args.due, args.user)
+        console.print(f"[bold green] Success! :[/bold green] Client '{project.owner}' posted project '{project.title}'")
     except Exception as e:
         console.print(f"[bold red] Operational Error:[/bold red] {e}")
