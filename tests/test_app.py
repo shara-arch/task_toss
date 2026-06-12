10704c363b6be199658399728d0cc5edb54ab35e
Sharon Moegi <sharamoegi@gmail.com>
2026-06-12 08:55:56 +0300

    Import dependencies

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+import pytest
+import os
+import json
+from unittest.mock import patch
+import utils.persistence
+from models.user import User
+from models.project import Project
+from models.task import Task
+
